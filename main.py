578166c3f9ff3f94353ee4a93f611b08a11e5f60
StephenTrainor <stephontrainor@gmail.com>
2022-10-15 20:59:10 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import sys
+import constants
+from flashcard import flashcard
+from parseInput import retrieve_flashcard_contents, get_korean, get_english, get_input
+
+
+def main():
+    mode = get_input(constants.START_MODE_PROMPT)
+
+    while mode.lower() not in constants.START_MODE_QUIT_OPTIONS:
+        if mode is '1':
+            add_to_flashcard_set()
+        elif mode is '2':
+            study_flashcard_set()
+        else:
+            print(constants.UNKNOWN_OPTION_ERROR)
+
+        mode = get_input(constants.START_MODE_PROMPT)
+
+
+def add_to_flashcard_set():
+    filename = get_input(constants.FLASHCARD_FILENAME_PROMPT)
+
+    with open(f"{constants.DEFAULT_FLASHCARD_DIRECTORY}{filename}.txt", "a+", encoding=constants.DEFAULT_FILE_ENCODING) as f:
+        korean_word = get_korean(constants.GET_KOREAN_PROMPT)
+        english_translation = get_english(constants.GET_ENGLISH_PROMPT)
+
+        while korean_word and english_translation:
+            f.write(f"{korean_word};{english_translation}\n")
+
+            korean_word = get_korean(constants.GET_KOREAN_PROMPT)
+            english_translation = get_english(constants.GET_ENGLISH_PROMPT)
+
+
+def study_flashcard_set():
+    korean_vocabulary_words = retrieve_flashcard_contents()
+    flashcard_set = flashcard(korean_vocabulary_words)
+
+    mode = get_input(constants.STUDY_FLASHCARD_SET_PROMPT)
+
+    while mode.lower() not in constants.STUDY_FLASHCARD_SET_QUIT_OPTIONS:
+        if mode is '1':
+            flashcard_set.display()
+
+        elif mode is '2':
+            flashcard_set.mock_test()
+
+        elif mode is '3':
+            flashcard_set.random_test()
+
+        elif mode is '4':
+            flashcard_set.mock_test(limited=True)
+
+        mode = get_input(constants.STUDY_FLASHCARD_SET_PROMPT)
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
